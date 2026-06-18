71dad856b6700f0eb8dd81855eeeb7148411bea5
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-06-18 06:42:50 -0500

    step 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,21 @@
-// src/App.tsx
+// src/components/App.tsx
 
-function Product() {
-  return (
-    <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-}
+import Product from './Product';
 
 export default function App() {
   return (
     <>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
+      <h1>Best selling</h1>
+       <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
     </>
   );
 }
